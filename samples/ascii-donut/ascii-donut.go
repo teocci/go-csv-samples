72922df85d0b3715ac3102c46cd445fbb99d3a5c
Legion5-Teocci <teocci@yandex.com>
2021-09-12 01:25:20 +0900

    tuned ascii donut parameters

--- samples/ascii-donut/ascii-donut.go
+++ samples/ascii-donut/ascii-donut.go
@@ -8,19 +8,25 @@ import (
 	"fmt"
 	"github.com/teocci/go-csv-samples/src/iolive"
 	"math"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 const (
 	luminance    = ".,-~:;=!*#$@"
 	thetaSpacing = 0.07
-	phiSpacing   = 0.02
+	phiSpacing   = 0.01
 
-	screenWidth  = 70
-	screenHeight = 30
+	screenWidth  = 50
+	screenHeight = 20
 
+	// R1 radius of circle
 	R1 = 1
+	// R2 center of circle
 	R2 = 2
+	// K2 Distance from object to screen
 	K2 = 5
 )
 
@@ -30,7 +36,7 @@ const (
 // is 3/4th of the way from the center to the side of the screen.
 // screenWidth*3/8 = K1*(R1+R2)/(K2+0)
 // screenWidth*K2*3/(8*(R1+R2)) = K1
-const K1 = screenWidth * K2 * 3 / (18 * (R1 + R2))
+const K1 = screenWidth * K2 * 3 / (16 * (R1 + R2))
 
 var (
 	output = make([][]rune, screenWidth)
@@ -46,14 +52,20 @@ func main() {
 	// start listening for updates and render
 	writer.Start()
 
-	for i := float64(0); i <= 100; i++ {
-		a, b = 0+0.7*i, 0+0.3*i
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		writer.Stop() // flush and stop rendering
+		os.Exit(1)
+	}()
+
+	for {
 		renderFrame(a, b)
-		time.Sleep(time.Millisecond * 80)
+		a += 0.04
+		b += 0.02
+		time.Sleep(time.Millisecond * 20)
 	}
-
-
-	writer.Stop() // flush and stop rendering
 }
 
 func renderFrame(a float64, b float64) {
@@ -65,7 +77,7 @@ func renderFrame(a float64, b float64) {
 		output[i] = make([]rune, screenHeight)
 	}
 	for i := range output {
-		for _ = range output[i] {
+		for range output[i] {
 			output[i] = append(output[i], ' ')
 		}
 	}
@@ -78,25 +90,25 @@ func renderFrame(a float64, b float64) {
 	// theta goes around the cross-sectional circle of a torus
 	for theta := float64(0); theta < 2*math.Pi; theta += thetaSpacing {
 		// precompute sines and cosines of theta
-		costheta := math.Cos(float64(theta))
-		sintheta := math.Sin(float64(theta))
+		cosTheta := math.Cos(theta)
+		sinTheta := math.Sin(theta)
 
 		// phi goes around the center of revolution of a torus
 		for phi := float64(0); phi < 2*math.Pi; phi += phiSpacing {
 			// precompute sines and cosines of phi
-			cosphi := math.Cos(phi)
-			sinphi := math.Sin(phi)
+			cosPhi := math.Cos(phi)
+			sinPhi := math.Sin(phi)
 
 			// the x,y coordinate of the circle, before revolving (factored
 			// out of the above equations)
-			circlex := R2 + R1*costheta
-			circley := R1 * sintheta
+			circleX := R2 + R1*cosTheta
+			circleY := R1 * sinTheta
 
 			// final 3D (x,y,z) coordinate after rotations, directly from
 			// our math above
-			x := circlex*(cosB*cosphi+sinA*sinB*sinphi) - circley*cosA*sinB
-			y := circlex*(sinB*cosphi-sinA*cosB*sinphi) + circley*cosA*cosB
-			z := K2 + cosA*circlex*sinphi + circley*sinA
+			x := circleX*(cosB*cosPhi+sinA*sinB*sinPhi) - circleY*cosA*sinB
+			y := circleX*(sinB*cosPhi-sinA*cosB*sinPhi) + circleY*cosA*cosB
+			z := K2 + cosA*circleX*sinPhi + circleY*sinA
 			ooz := 1 / z // "one over z"
 
 			// x and y projection.  note that y is negated here, because y
@@ -117,18 +129,18 @@ func renderFrame(a float64, b float64) {
 			}
 
 			// calculate luminance.  ugly, but correct.
-			l := cosphi*costheta*sinB - cosA*costheta*sinphi - sinA*sintheta + cosB*(cosA*sintheta-costheta*sinA*sinphi)
+			l := cosPhi*cosTheta*sinB - cosA*cosTheta*sinPhi - sinA*sinTheta + cosB*(cosA*sinTheta-cosTheta*sinA*sinPhi)
 			// l ranges from -sqrt(2) to +sqrt(2).  If it's < 0, the surface
 			// is pointing away from us, so we won't bother trying to plot it.
 			if l > 0 {
-				// test against the z-buffer.  larger 1/z means the pixel is
+				// test against the z-buffer larger 1/z means the pixel is
 				// closer to the viewer than what's already plotted.
 				//fmt.Printf("(%d, %d)\n", xp, yp)
 				if ooz > zBuff[xp][yp] {
 					zBuff[xp][yp] = ooz
 					luminanceIndex := int(l * 8)
 					// luminanceIndex is now in the range 0..11 (8*sqrt(2) = 11.3)
-					// now we lookup the character corresponding to the
+					// now we look up the character corresponding to the
 					// luminance and plot it in our output:
 
 					//_, _ = fmt.Fprintf(writer, "(%d, %d) | %d\n", xp, yp, luminanceIndex)
